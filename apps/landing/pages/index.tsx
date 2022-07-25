0cbe2a01fa9561bceab05f7fb0bc0cc54fbc2bef
kostyamospan <kostya04042001@gmail.com>
2022-07-25 22:38:42 +0300

    chore: footer icons and fonts

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,14 +1,17 @@
 import { Box } from '@pearlydrive/core-ui';
-import { Text } from '@pearlydrive/landing-ui';
 import type { NextPage } from 'next';
+
 import { Footer } from '../components';
 
 const Home: NextPage = () => {
   return (
-    <Box background='#120021' style={{
-      maxWidth: '1920px',
-      margin: 'auto'
-    }}>
+    <Box
+      background="#120021"
+      style={{
+        maxWidth: '1920px',
+        margin: 'auto',
+      }}
+    >
       <Footer></Footer>
     </Box>
   );
